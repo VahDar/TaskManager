bb8986b6b9b77d2cf952356b4b09c4cca6f79a04
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-04-30 20:06:26 +0300

    create xib and category vc

---
+++ ToDoApp/CategoryViewControllers/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  ToDoApp
+//
+//  Created by Vakhtang on 30.04.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
